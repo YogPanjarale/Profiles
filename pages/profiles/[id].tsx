38cba1ee123643dfaf2d0470804e3d4b35691d8b
YogPanjarale <yog.panjarale@gmail.com>
2021-04-03 23:34:27 +0530

    Revert "getInitialProps"
    This reverts commit 4d3a8fda664d47a880038319deb6f929965be7e2.

--- pages/profiles/[id].tsx
+++ pages/profiles/[id].tsx
@@ -9,51 +9,40 @@ export default function Id({ data }: { data: IProfile }) {
   const router = useRouter();
   // console.log(data);
   const { id } = router.query;
-  const {
-    title,
-    links,
-    image = "https://img.icons8.com/pastel-glyph/2x/person-male--v3.png",
-  } = data;
+  const { title, links,image="https://img.icons8.com/pastel-glyph/2x/person-male--v3.png" } = data;
 
   return (
-    <div className="bg-gradient-to-r from-green-400 to-blue-500 py-10 flex-col h-screen align-items flex">
+    <div className="bg-gradient-to-r from-green-400 to-blue-500 py-10 flex-col h-screen align-items flex" >
       <Head>
         <title>{title} | Links</title>
         <link rel="icon" href={image} />
-        <link
-          href="https://fonts.googleapis.com/css?family=Roboto"
-          rel="stylesheet"
-        ></link>
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"></link>
       </Head>
       <div className="text-center py-2 space-y-2">
-        <img
-          src={image}
-          className="w-28 h-28 self-center rounded-full mx-auto my-auto border-white border-2"
-        />
+      <img src={image} className="w-28 h-28 self-center rounded-full mx-auto my-auto border-white border-2"/>
         <h1 className="font-medium  text-xl ">@{title}</h1>
       </div>
       <div className="space-y-5 m-10 align-middle ">
-        {links.map((d, j) => (
-          <MyLink key={j} {...d} />
-        ))}
+      {links.map((d, j) => (
+        <MyLink key={j} {...d} />
+      ))}
       </div>
-      <Rights />
+      <Rights/>
     </div>
   );
 }
 
-// export async function getServerSideProps({ params }) {
-export async function getInitialProps({ params }) {
+export async function getServerSideProps({ params }) {
   // const req = await fetch(`/profiles.json`);
-  const req = await fetch(
-    `https://raw.githubusercontent.com/YogPanjarale/Profiles/main/public/profiles.json`
-  );
+  const req = await fetch(`https://raw.githubusercontent.com/YogPanjarale/Profiles/main/public/profiles.json`);
   // console.log(params,req)
   let result = await req.json();
-  if (params.id.toString() in result) {
+  if (params.id.toString() in result){
+
     result = result[params.id.toString()];
-  } else {
-    result = result["none"];
+  }
+  else{
+    result = result["none"]
   }
   return {
     props: { data: result },
