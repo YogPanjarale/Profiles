0dc7b0fc90437d636cb304fa76884eed50266cc4
YogPanjarale <yog.panjarale@gmail.com>
2021-04-08 18:45:35 +0530

    comments removed

--- pages/profiles/[id].tsx
+++ pages/profiles/[id].tsx
@@ -71,20 +71,6 @@ export default function Id({ data }: { data: IProfile }) {
 }
 
 export async function getServerSideProps({ params }:{params:{id:string}}) {
-  // const req = await fetch(`/profiles.json`);
-  // const req = await fetch(
-  //   `https://raw.githubusercontent.com/YogPanjarale/Profiles/main/public/profiles.json`
-  // );
-  // // console.log(params,req)
-  // let result = await req.json();
-  // if (params.id.toString() in result) {
-  //   result = result[params.id.toString()];
-  // } else {
-  //   result = result["none"];
-  // }
-
-  // const req:Response = await fetch(`https://localhost:3000/api/${params.id.toString()}`)
-  // let result =req.json()
   let result =await  getUser(params.id.toString())
   return {
     props: { data: result },
