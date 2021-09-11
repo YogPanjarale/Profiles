ae0915efd5eb0ea982b975a2754fb8b34d961e73
Yog <yog.panjarale@gmail.com>
2021-09-11 11:22:18 +0530

    made home page as yog by default

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,25 @@
 import Head from "next/head";
 import { MyForm } from '../components/form'
-export default function Page() {
-    return (
-        <div className="pt-12">
-            <Head>
-                <title>Login Form</title>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <MyForm onSubmit={(value)=>{ console.log(value)}}/>
-        </div>
-    )
+import ID from './profiles/[id]'
+
+export default function Page({data}) {
+    return <Id data={data}></Id>
+    // return (
+    //     <div className="pt-12">
+    //         <Head>
+    //             <title>Login Form</title>
+    //             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+    //             <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    //             <link rel="icon" href="/favicon.ico" />
+    //         </Head>
+    //         <MyForm onSubmit={(value)=>{ console.log(value)}}/>
+    //     </div>
+    // )
 }
+export async function getServerSideProps({ params }:{params:{id:string}}) {
+    let result =await  getUser('yog')
+    return {
+      props: { data: result },
+    };
+  }
+  
